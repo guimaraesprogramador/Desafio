d84bf09cca523e37e40ad3648161462f5fec6afd
kevin <guimaraesprogramador@gmail.com>
2020-09-03 16:02:06 -0300

    configurado a rsa e falta a segunda pagina para fazer de acordo com o esboço.

--- src/angular.js
+++ src/angular.js
@@ -1,14 +1,82 @@
-var app = angular.module('Desafio',['ngBrowser','ngCordova'])
-app.controller('Contra-IA',['$scope','appBrowser',
-function($scope,appBrowser){
-$scope.verificar_plataforma = function(){
-    var theads  = []
-    if(appBrowser.getBrowserInfo().platform == "Android"){
-     
-    }
-    else{
+var theads  = []
+var valor = [];
+
+window.onload = function(){
+    document.body.style.visibility ="hidden";
+    if(navigator.onLine){
+        document.body.style.visibility = "visible";
+        theads.push(new Worker("./src/rsa.js"));
+        theads[0].postMessage("começo");
+        theads[0].onmessage = function(ev){
+            if(ev.data.criptografia.length == 0){
+                location.reload(true);
+            }
+            else{
+              ev.data.link =   
+              window.location.origin +"/login.html"+
+              
+              "?p="+ev.data.primo1 +
+              "&q="+ev.data.primo2;
+             valor.push(ev.data.criptografia[0].chave_publica,
+                ev.data.criptografia[0].chave_privada, 
+                ev.data.criptografia[0].mod,
+                ev.data.link);
+                theads.pop();
     
+            }
+            
+        }
     }
+
+}
+var app = angular.module('Desafio',['ngBrowser'])
+
+app.controller('Contra-IA',['$scope','appBrowser',
+
+function($scope,appBrowser){
+    
+$scope.verificar_plataforma = function(){
+    
+    var elementos_navagador ={
+        platform:appBrowser.getBrowserInfo().platform,
+        name:appBrowser.getBrowserInfo().name
+    }
+    return new Promise(function(resp,reject){
+    if(elementos_navagador.platform == "Android"  || elementos_navagador.name == "Google Chrome"  ){
+
+        modulos().artificial().then(r=>{
+           resp(r);
+        }).catch(e=>{
+ 
+        })
+    
+        
+        
+     }else{
+        resp("sem permissão");
+     }
+})
+    
 };
-$scope.verificar_plataforma();
+$scope.carregar_dados = function(){
+var iniciar = document.getElementsByTagName("button")[0];
+iniciar.addEventListener("click",function(ev){
+    $scope.verificar_plataforma().then(p=>{
+        if(p == "permitido"){
+         window.location.assign(valor[3]);
+    
+        }
+        else console.log(p);
+    })
+})
+var pontos = document.getElementsByTagName("button")[1];
+pontos.addEventListener("click",function(ev){
+console.log(pontos.value);
+})
+var quem_somos = document.getElementsByTagName("button")[2];
+quem_somos.addEventListener("click",function(ev){
+    console.log(quem_somos.value)
+})
+
+}
 }]);
--- src/rsa.js
+++ src/rsa.js
@@ -1,30 +1,99 @@
 class rsa{
     constructor(){
-        this.resposta_link;    
+         
     }
-    link;
+    link = "";
     get link(){
-        return this.resposta_link;
+        return this.link;
     }
     set link(tipo){
-        this.resposta_link = tipo;
+        this.link = tipo;
     }
-    mmc(){
-        return 0;
+    fatorial_primo = function(z){
+        var r = 1;
+        var e = 0;
+                while(r<=z){
+                    e = r * z;
+                    if(e % z ==1){
+                        break;
+                    }
+                    else e = 0;
+                    r++;
+                }
+        
+        return r;
+    }
+    primo = function(){
+        var c = 0;
+        var x = 0;
+        var b = 0;
+        // 0 até 100
+        c = Math.floor(Math.random() *101);
+        b = this.fatorial_primo(c);
+                for(var i = 1;i<=b;i++){
+                    if(this.Divisaoexata(b,i)){
+                        x++;
+                    }
+                }
+        if(x ==2){
+            return b;
+        }
+        else return 0;
+    }
+    Euler(){
+    
+
+    var anterior = this.primo(); 
+      var atual = this.primo();
+      var resto = anterior % atual;
+        while(resto!= 0 && resto.toString() != NaN.toString() &&resto != Infinity){
+            anterior = atual;
+            atual = resto;
+            resto = anterior % atual;
+        }
+        return atual;
     }
     Url(){
-        
-        return null;
+       
+       // 0 até 100;
+         
     }
     artificial(){
-        navigator.mediaDevices.getUserMedia({audio:true}).then(r=>{
-            var m = new SpeechSynthesisUtterance("oi");
-            m.lang = "pt-br";
-            var ia = window.speechSynthesis.speak(m);
+        return new Promise(function(response,rejec){
+            navigator.mediaDevices.getUserMedia({audio:true}).then(r=>{
+                response("permitido")
+             })
         })
+       
+        
+        
     }
     Regras(){
-        return [];
+       var valores = [];
+       var valor = false;
+       var d,e = 0;
+       var n;
+       var z;
+       do{  
+        this.p = Math.floor(Math.random() *100);
+        this.q = Math.floor(Math.random() *100);    
+        n = this.p * this.q;
+        z = (this.p-1)*(this.q-1);
+       // chave privada
+       d = this.primo();
+       // chave publica 
+       e = this.Euler();
+       valor = (d * e)%z;
+       if(valor == 1)valor = true;
+       else valor = false;
+       
+        }while( valor==false|| d==e);
+        valores.push({chave_publica:e,chave_privada:d, mod:n});
+        return valores;
+        
+    }
+    Divisaoexata(num,i){
+        return (num % i) ==0;
     }
     modulos_média(){
 
@@ -37,8 +106,18 @@ class rsa{
     }
 }
 function modulos(){
-
+const r = new rsa();
+return r;
 }
-self.addEventListener("message",function(ev){
 
+self.addEventListener("message",function(ev){
+var tipo = ev.data;
+var rsa = modulos();
+switch(tipo){
+    case "começo":
+   var array =  rsa.Regras();
+    this.postMessage({criptografia:array,primo1:rsa.p,primo2:rsa.q,link:rsa.link});
+    this.self.close();
+    break;
+}
 })
